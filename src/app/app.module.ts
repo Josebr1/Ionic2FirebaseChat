017288564c5026ee2b8309ad705ebcaef3566a68
José Antônio <jose.silva.br@outlook.com>
2017-12-15 23:02:01 -0200

    version compatible with course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,18 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2/angularfire2';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {HttpModule} from "@angular/http";
+
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyBa5gcDij3VODKo8va7RmSjfZdqPTinZ6I",
+  authDomain: "ionic2-firebase-chat-38a04.firebaseapp.com",
+  databaseURL: "https://ionic2-firebase-chat-38a04.firebaseio.com",
+  storageBucket: "ionic2-firebase-chat-38a04.appspot.com",
+};
 
 @NgModule({
   declarations: [
@@ -14,6 +24,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseAppConfig),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
