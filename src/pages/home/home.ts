359d0e70c0d875b8752dd09fe76869c70137671b
José Antônio <jose.silva.br@outlook.com>
2017-12-15 23:28:51 -0200

    Listando usuários cadastrados

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {SignupPage} from "../signup/signup";
+
+import { SignupPage } from '../signup/signup';
+
+import { FirebaseListObservable } from 'angularfire2';
+
+import { User } from '../../models/user.model';
+import { UserService } from '../../providers/user/user.service';
 
 @Component({
   selector: 'page-home',
@@ -8,12 +14,23 @@ import {SignupPage} from "../signup/signup";
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  users: FirebaseListObservable<User[]>;
+
+  constructor(public navCtrl: NavController, public userService: UserService) {
 
   }
 
+  ionViewDidLoad(){
+    this.users = this.userService.users;
+  }
+
+
   onSignUp(): void{
     this.navCtrl.push(SignupPage);
+  }
+
+  onChatCreate(user: User){
+    console.log(user);
   }
 
 }
--- src/providers/user/user.service.ts
+++ src/providers/user/user.service.ts
@@ -2,20 +2,24 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-import { AngularFire } from 'angularfire2';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 import { User } from '../../models/user.model';
 
 @Injectable()
 export class UserService {
 
+  users: FirebaseListObservable<User[]>;
+
   constructor(public af: AngularFire,
               public http: Http) {
     console.log('Hello UserProvider Provider');
+
+    this.users = this.af.database.list(`/users`);
   }
 
   create(user: User): firebase.Promise<void>{
-    return this.af.database.list(`/users`)
+    return this.users
       .push(user);
   }
 
