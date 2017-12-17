a09c53024aa8bb2e5b619568d4956496891a25ce
José <jose.silva.br@outlook.com>
2017-12-17 02:07:08 -0200

    Usuário atual no UserService

--- src/providers/user/user.service.ts
+++ src/providers/user/user.service.ts
@@ -1,3 +1,4 @@
+import { FirebaseAuthState } from 'angularfire2';
 import { User } from './../../models/user.model';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
@@ -8,11 +9,13 @@ import { BaseService } from '../base/base.service';
 
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Injectable()
 export class UserService extends BaseService{
 
   users: FirebaseListObservable<User[]>;
+  currentUser: FirebaseObjectObservable<User>;
 
   constructor(public af: AngularFire,
               public http: Http) {
@@ -20,6 +23,16 @@ export class UserService extends BaseService{
     console.log('Hello UserProvider Provider');
 
     this.users = this.af.database.list(`/users`);
+    this.listenAuthState();
+  }
+
+  private listenAuthState(): void{
+    this.af.auth
+    .subscribe((authState: FirebaseAuthState) => {
+      if(authState){
+        this.currentUser = this.af.database.object(`/users/${authState.auth.uid}`);
+      }
+    });
   }
 
   create(user: User, uuid: string): firebase.Promise<void>{
