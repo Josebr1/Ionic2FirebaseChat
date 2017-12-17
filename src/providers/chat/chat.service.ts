731246e3ea90b0a4d4f03ff8a7085a3615379a36
José <jose.silva.br@outlook.com>
2017-12-17 02:37:54 -0200

    Retornando um Chat específico

--- src/providers/chat/chat.service.ts
+++ src/providers/chat/chat.service.ts
@@ -1,12 +1,27 @@
+import { FirebaseObjectObservable } from 'angularfire2/database';
+import { Chat } from './../../models/chat.model';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { BaseService } from '../base/base.service';
+import { AngularFire } from 'angularfire2';
 
 @Injectable()
-export class ChatService {
+export class ChatService extends BaseService {
 
-  constructor(public http: Http) {
-    console.log('Hello ChatProvider Provider');
+  constructor(public http: Http, public af: AngularFire) {
+    super();
+  }
+
+  create(chat: Chat, userId1: string, userId2: string): firebase.Promise<void> {
+    return this.af.database.object(`/chats/${userId1}/${userId2}`)
+      .set(chat)
+      .catch(this.handlePromiseError);
+  }
+
+  getDeepChat(userId1: string, userId2: string): FirebaseObjectObservable<Chat>{
+    return <FirebaseObjectObservable<Chat>> this.af.database.object(`/chats/${userId1}/${userId2}`)
+    .catch(this.handleObservableError);
   }
 
 }
