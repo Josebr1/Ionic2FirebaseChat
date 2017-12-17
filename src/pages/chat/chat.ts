bcc9316dde94d13ada35fbf3e9fcfb48610a8948
José <jose.silva.br@outlook.com>
2017-12-17 02:24:05 -0200

    Título do chat, recipient e sender users

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,6 +1,8 @@
+import { User } from './../../models/user.model';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthService } from '../../providers/auth/auth.service';
+import { UserService } from '../../providers/user/user.service';
 
 @IonicPage()
 @Component({
@@ -10,8 +12,11 @@ import { AuthService } from '../../providers/auth/auth.service';
 export class ChatPage {
 
   messages: string[] = [];
+  pageTitle: string;
+  sender: User;
+  recipient: User;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public authService: AuthService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authService: AuthService, public userService: UserService) {
   }
 
   ionViewCanEnter(): Promise<boolean> {
@@ -20,6 +25,13 @@ export class ChatPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChatPage');
+    this.recipient = this.navParams.get('recipientUser');
+    this.pageTitle = this.recipient.name;
+    this.userService.currentUser
+    .first()
+    .subscribe((currentUser: User) => {
+      this.sender = currentUser;
+    });
   }
 
   sendMessage(newMessage: string){
