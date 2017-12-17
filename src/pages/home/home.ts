558f61bb0f4ca442c9ef909475cf394f23aafa2c
José <jose.silva.br@outlook.com>
2017-12-17 02:52:15 -0200

    Criando novos chats

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { Chat } from './../../models/chat.model';
 import { AuthService } from './../../providers/auth/auth.service';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
@@ -9,6 +10,9 @@ import { FirebaseListObservable } from 'angularfire2';
 import { User } from '../../models/user.model';
 import { UserService } from '../../providers/user/user.service';
 import { ChatPage } from '../chat/chat';
+import { ChatService } from '../../providers/chat/chat.service';
+
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -19,27 +23,52 @@ export class HomePage {
   users: FirebaseListObservable<User[]>;
   view: string = 'chats';
 
-  constructor(public authService: AuthService, public navCtrl: NavController, public userService: UserService) {
+  constructor(public authService: AuthService,
+    public chatService: ChatService,
+    public navCtrl: NavController,
+    public userService: UserService) {
 
   }
 
   ionViewCanEnter(): Promise<boolean> {
     return this.authService.authenticated;
-   }
+  }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     this.users = this.userService.users;
   }
 
 
-  onSignUp(): void{
+  onSignUp(): void {
     this.navCtrl.push(SignupPage);
   }
 
-  onChatCreate(user: User){
-    console.log(user);
+  onChatCreate(recipientUser: User) {
+    console.log(recipientUser);
+
+    this.userService.currentUser
+      .first()
+      .subscribe((currentUser: User) => {
+        this.chatService.getDeepChat(currentUser.$key, recipientUser.$key)
+        .first()
+        .subscribe((chat: Chat) => {
+          console.log(chat);
+
+          if(chat.hasOwnProperty('$value')){
+            let timestamp: Object = firebase.database.ServerValue.TIMESTAMP;
+
+            let chat1 = new Chat('', timestamp, recipientUser.name, '');
+            this.chatService.create(chat1, currentUser.$key, recipientUser.$key);
+
+            let chat2 = new Chat('', timestamp, currentUser.name, '');
+            this.chatService.create(chat2, recipientUser.$key, currentUser.$key);
+          }
+
+        });
+      });
+
     this.navCtrl.push(ChatPage, {
-      recipientUser: user
+      recipientUser: recipientUser
     });
   }
 
